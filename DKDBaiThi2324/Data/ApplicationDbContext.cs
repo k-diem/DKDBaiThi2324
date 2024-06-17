8e768b37bdda932c268c208ef2e52f3675c0fe92
k-diem <142491589+k-diem@users.noreply.github.com>
2024-06-17 13:16:22 +0700

    DKD130TaoDB

---
+++ DKDBaiThi2324/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using DKDBaiThi2324.Models;
+namespace DKDBaiThi2324.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+    public DbSet<Person> Person {get; set;}
+    
+   
+
+
+}
+}
