de62c0eb901b196c18d67d639dc1200ec319976b
k-diem <142491589+k-diem@users.noreply.github.com>
2024-06-17 13:09:22 +0700

    DKD130Person

---
+++ DKDBaiThi2324/Models/Person.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+// DUONG KHANH DIEM - 2021050130
+namespace DKDBaiThi2324.Models
+{
+    [Table("Persons")]
+    public class Person
+    {
+        [Key]
+        public string PersonId { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+    }
+}
+
